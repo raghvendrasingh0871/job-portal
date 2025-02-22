e2d9a7a801af6f614b545a2da284c6eb79f6dc54
raghvendrasingh0871 <raghvendrasingh0871@gmail.com>
2025-02-22 23:38:03 +0530

    database-connected

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async ()=>{
     try{
-        const connection = await mongoose.connect();
+        const connection = await mongoose.connect(process.env.MONGO_URL);
     }
     catch(error){
         console.log("found error while connecting");
